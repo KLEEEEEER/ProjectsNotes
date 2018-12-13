cf883d7a0c76a57b67dd531555605ca8243bdfcb
Maxim <gonzalx_747@mail.ru>
2018-12-14 00:34:55 +0900

    Added search views and search function

--- ProjectsNotes.py
+++ ProjectsNotes.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
+import io
 import datetime
 import sys
 from PyQt5.QtWidgets import QInputDialog, QApplication, QWidget, QPushButton, QLabel, QLineEdit, QComboBox, QTextEdit, QGridLayout
@@ -28,6 +29,7 @@ class ProjectsNotes(QWidget):
         global nameEdit
         global noteContent
         global selectBox
+        global searchInput
 
         progName = QLabel(u'Projects Notes', self)
 
@@ -70,6 +72,12 @@ class ProjectsNotes(QWidget):
         rowsCount = QLabel('Текст:')
         noteContent = QTextEdit()
 
+        searchButton = QPushButton('Найти', self)
+        search = QLabel('Поиск')
+        searchInput = QLineEdit()
+        searchContent = QTextEdit()
+        searchButton.clicked.connect(self.searchStringButton)
+
        # nameEdit.textChanged[str].connect(ChangedName)
         #noteContent.textChanged[str].connect(ChangedName)
 
@@ -84,6 +92,10 @@ class ProjectsNotes(QWidget):
         grid.addWidget(selectBox, 3, 1)
         grid.addWidget(progName, 4, 1)
         grid.addWidget(dobtn, 5, 1)
+        grid.addWidget(search, 1, 3)
+        grid.addWidget(searchInput, 1, 4)
+        grid.addWidget(searchContent, 2, 3, 5, 2)
+        grid.addWidget(searchButton, 1, 5)
 
         grid.addWidget(qbtn, 3, 2)
         grid.addWidget(copyright_string, 8, 1)
@@ -91,7 +103,7 @@ class ProjectsNotes(QWidget):
 
         self.setLayout(grid)
 
-        self.setGeometry(300, 300, 450, 200)
+        self.setGeometry(300, 300, 600, 200)
         self.setWindowTitle('Projects Notes v' + self.version)
         self.setWindowIcon(QIcon('icon.png'))
         self.show()
@@ -101,7 +113,7 @@ class ProjectsNotes(QWidget):
         try:
             os.makedirs(self.projects_folder_name)
         except OSError:
-            progName.setText(u'Загружены проекты из папки ' + self.projects_folder_name)
+            progName.setText(u'Ошибка загрузки папок в ' + self.projects_folder_name)
 
         projects = []
         projects_array = os.listdir(self.projects_folder_name)
@@ -150,6 +162,21 @@ class ProjectsNotes(QWidget):
         document.close()
         progName.setText(u'Сохранён файл ' + file_name)
 
+    def searchStringButton(self):
+        self.searchString(searchInput.text())
+
+    def searchString(self, string):
+        projects = self.getProjects()
+        searchResult = []
+        for project in projects:
+            if os.path.isdir(self.projects_folder_name + '/' + project):
+                for file in os.listdir(self.projects_folder_name + '/' + project):
+                    if file.endswith('.txt'):
+                        with io.open(self.projects_folder_name + '/' + project + '/' + file, encoding='utf-8') as file_txt:
+                            for line in file_txt:
+                                if string in line:
+                                    searchResult.append(self.projects_folder_name + '/' + project + '/' + file)
+        return searchResult
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
